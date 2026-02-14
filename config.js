c3627976f55a660d41087943333dd7fadcf09c1d
exoticexes <mertrobloxtr@gmail.com>
2026-02-15 01:31:52 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     host: process.env.HOST || 'play.everyones.run.place',
     port: parseInt(process.env.PORT) || 24107,
-    version: process.env.VERSION || '1.21.1'
+    version: process.env.VERSION || '1.21.11'
   },
   bot: {
     username: process.env.BOT_NAME || 'ExEB0t',
